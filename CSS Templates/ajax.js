74abb16db919ae4d0e2f931391075edfc210f1a4
bettygha <bettygha2011@yahoo.com>
2023-03-17 14:00:43 -0500

    Index

---
+++ CSS Templates/ajax.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+
+    $("#button").click(clicked);
+    $("#word").on("keyup", action);
+
+}
+function action(e) {
+    if ($(this).val().length > 0) {
+        $("#button").prop("disabled", false);
+        $("#button").css("background-color","#55cbff");
+    }
+    else {
+        $("#button").prop("disabled", true);
+        $("#button").css("background-color","#c4edff");
+        $(".SG").empty();
+    }
+    e.preventDefault();
+
+}
+function clicked(e) {
+    alert("clicked");
+    let wordToBeSearched = $("#word").val();
+    $.ajax({
+        url: "http://localhost:3000/action?action=fetch",
+        method: "GET",
+        dataType: "json",
+        "success": function (data) {
+            alert("hey")
+            let html = "";
+            let count = 1;
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].word.toLowerCase() == wordToBeSearched.toLowerCase()) {
+                    let classOfList = "s"+String(count);
+                    html += `<li class="sgLi">
+                                <div class="box">
+                                   <h3>${data[i].wordtype}</h3>
+                                   <ul class=${classOfList}>
+                                        <li>${data[i].definition}</li> 
+                                     </ul>
+                                </div>
+                            </li>`
+                    count = count + 1;
+                }
+                
+            }
+            $(".SG").html(html);
+        },
+        "error": function (err) {
+            alert(err.responseText);
+        }
+    })
+    e.preventDefault();
+
+
+}
+
--- Database/ajax.js
+++ Database/ajax.js
@@ -1,19 +1,40 @@
 window.onload=function(){
-  $("#result").click(clicked);
+   
+   $("#result").click(clicked);
+   $("#word").on("keyup",action);
+
 }
-function clicked(){
+function action(e){
+    if($(this).val().length >0){
+        $("#result").prop("disabled",false);
+    }
+    else{
+        $("#result").prop("disabled",true);
+    }
+    e.preventDefault();
+    
+}
+function clicked(e){
+    alert("clicked");
+    let wordToBeSearched = $("#word").val();
     $.ajax({
         url:"http://localhost:3000/action?action=fetch",
         method:"GET",
         dataType:"json",
         "success":function(data){
             alert("hey")
-            $("#result2").text(data[0].first_name); 
+            let html ="";
+            for(let i = 0 ; i <data.length ;i++){
+                if(data[i].word.toLowerCase() == wordToBeSearched.toLowerCase()){
+                    html +=`<dt>${data[i].wordtype}</dt><dd>${data[i].definition}</dd>`
+            }}
+            $("#result2").html(html); 
         },
         "error":function(err){
             alert(err.responseText);
         }
     })
+    e.preventDefault();
     
     
 }
