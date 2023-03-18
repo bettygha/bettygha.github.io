1fb31eda5645eefa0fca8e76fd3fbd0091ae876f
bettygha <bettygha2011@yahoo.com>
2023-03-18 11:03:14 -0500

    Express File Deleted

--- Express/HelloWorl.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-// using the app we are configuring the route of 'GET' and pasth is '/'
-//when ever request for this endpoint => Hello world
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-});
-app.listen(3000);
